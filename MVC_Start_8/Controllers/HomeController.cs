35fef4de4c832031652c809cba58389b9f225a94
deepa510 <deepabiswas821@gmail.com>
2024-04-16 08:45:49 -0400

    changes

--- MVC_Start_8/Controllers/HomeController.cs
+++ MVC_Start_8/Controllers/HomeController.cs
@@ -15,6 +15,10 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult DIS()
+    {
+        return View();
+    }
 
     public IActionResult Contact()
     {
